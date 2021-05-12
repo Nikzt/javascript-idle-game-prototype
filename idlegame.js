74ed2e687dabd72c6baaaadaa00b0336c6ada352
Bence Linder <bence.linder@gmail.com>
2021-05-12 19:20:37 -0400

    updated idlegame.js

--- idlegame.js
+++ idlegame.js
@@ -1,1 +1,28 @@
-alert("fuck you bram")
+const resourceView = document.getElementById("resource-view")
+const resourceList = document.createElement("ul")
+const resourceListItemA = document.createElement("li")
+resourceListItemA.textContent = "A: 0"
+const resourceListItemB = document.createElement("li")
+resourceListItemB.textContent = "B: 0"
+
+resourceView.appendChild(resourceList)
+resourceList.appendChild(resourceListItemA)
+resourceList.appendChild(resourceListItemB)
+
+/**
+ * Sets the value of resource A to the specified number,
+ * and updates the text content of the resource A display
+ * @param {number} newResourceAValue the value of resource A
+ */
+function updateResourceA(newResourceAValue) {
+
+}
+
+/**
+ * Sets the value of resource B to the specified number,
+ * and updates the text content of the resource B display
+ * @param {number} newResourceBValue the value of resource B
+ */
+function updateResourceB(newResourceBValue) {
+
+}
