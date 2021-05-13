90dc46a9feb4953c4a6555125c1c9ebea5bfc3c4
Bence Linder <bence.linder@gmail.com>
2021-05-12 20:05:24 -0400

    resource generators and stuff

--- idlegame.js
+++ idlegame.js
@@ -36,6 +36,29 @@ function updateResourceB(newResourceBValue) {
 
 const addOne = (a) => a + 1
 
+const resourceGeneratorView = document.getElementById("resource-generators-view")
+const resourceGeneratorsList = document.createElement("ul")
+const resourceGeneratorAItem = document.createElement("li")
+resourceGeneratorAItem.textContent = "A Generators: 0"
+const resourceGeneratorBItem = document.createElement("li")
+resourceGeneratorBItem.textContent = "B Generators: 0"
+
+resourceGeneratorView.appendChild(resourceGeneratorsList)
+resourceGeneratorView.appendChild(resourceGeneratorAItem)
+resourceGeneratorView.appendChild(resourceGeneratorBItem)
+
+let resourceGeneratorsA = 0;
+let resourceGeneratorsB = 0;
+
+function updateResourceGeneratorA(newResourceGeneratorAValue) {
+    resourceGeneratorsA = newResourceGeneratorAValue;
+    resourceGeneratorAItem.textContent = "A Generators: " + newResourceGeneratorAValue
+}
+
+function updateResourceGeneratorB(newResourceGeneratorBValue) {
+    resourceGeneratorsB = newResourceGeneratorBValue;
+    resourceGeneratorBItem.textContent = "B Generators: " + newResourceGeneratorBValue
+}
 
 document.getElementById("plus-one-a-button")
     .addEventListener("click", () => updateResourceA(addOne(resourceA)), false)
@@ -47,4 +70,20 @@ document.getElementById("plus-one-b-button")
         else alert('Not enough Resource A!')
     }, false)
 document.getElementById("plus-one-a-gen-button")
+    .addEventListener("click", () => {
+        if (resourceA >= 50) {
+            updateResourceGeneratorA(resourceGeneratorsA + 1)
+            updateResourceA(resourceA - 50)
+        } else {
+            alert("not enough vespene gas")
+        }
+    }, false)
 document.getElementById("plus-one-b-gen-button")
+    .addEventListener("click", () => {
+        if (resourceB >= 100) {
+            updateResourceGeneratorB(resourceGeneratorsB + 1)
+            updateResourceB(resourceB - 100)
+        } else {
+            alert("not enough gay monkey cum")
+        }
+    }, false)
